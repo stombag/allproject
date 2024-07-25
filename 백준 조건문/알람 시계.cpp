6ef16d6170d0069fc7a3263504047ebd8d3c92fd
KMG <ngl570202@gmail.com>
2024-07-25 20:08:25 +0900

    백준

---
+++ 백준 조건문/알람 시계.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std ;
+int main (){
+    int a, b;
+    cin >> a >> b;
+    if(b < 45){
+        b += 15;
+        if(--a < 0)
+        	a = 23;
+    }
+    else
+    
+        b -= 45;
+
+    cout << a << " "<< b;
+             
+        
+        return 0 ;
+}
