c9f5f6c54ff7e61d953899d32e5856cdaa7f5268
KMG <ngl570202@gmail.com>
2024-07-31 00:05:38 +0900

    백준

---
+++ 백준 반복문/A+B - 3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std; 
+int main() {
+    int A, B, C;
+    cin>> A;
+    for(int i=0; i <A; i++){
+        
+        cin>> B>> C;
+        cout<<B+C<<"\n";
+        
+    }
+    return 0;
+}
