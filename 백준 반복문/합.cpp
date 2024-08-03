20c759ca44295cff72074f2c3241e9befae1aa96
KMG <ngl570202@gmail.com>
2024-08-04 04:29:09 +0900

    백준

---
+++ 백준 반복문/합.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+
+using namespace std;
+
+int main (){
+    int A,B = 0 ;
+    cin >> A ;
+    for(int i = 1 ; i<=A ; i++){
+            
+            B += i;
+           
+        }
+
+        cout<< B ;
+    return 0; 
+}
