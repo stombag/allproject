ebce74c44537688cd68a6b2dad02b7bec40db3c5
KMG <ngl570202@gmail.com>
2024-08-08 04:45:21 +0900

    백준

--- 백준 조건문/알람 시계.cpp
+++ 백준 조건문/알람 시계.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 
-using namespace std ;
-int main (){
+using namespace std;
+int main()
+{
     int a, b;
     cin >> a >> b;
-    if(b < 45){
+    if (b < 45)
+    {
         b += 15;
-        if(--a < 0)
-        	a = 23;
+        if (--a < 0)
+            a = 23;
     }
     else
 
         b -= 45;
 
-    cout << a << " "<< b;
-             
-        
-        return 0 ;
+    cout << a << " " << b;
+
+    return 0;
 }
 /* 조금 생각해야함 */
