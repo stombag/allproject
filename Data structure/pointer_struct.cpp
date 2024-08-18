cea7ed55cfb27d8afbe92c6b7a0453039136f137
KMG <ngl570202@gmail.com>
2024-08-18 13:39:36 +0900

    구조체 포인터 연결하기

---
+++ Data structure/pointer_struct.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    struct Node
+    {
+
+        int data;
+        Node *next;
+    };
+    Node *head;
+    Node n1;
+    Node n2;
+    Node n3;
+
+    head = &n1;
+    n1.next = &n2;
+    n2.next = &n3;
+
+    n1.data = 1;
+    n2.data = 2;
+    n3.data = 3;
+    cout << head->data;
+    cout << head->next->data;
+    cout << head->next->next->data;
+    return 0;
+}
