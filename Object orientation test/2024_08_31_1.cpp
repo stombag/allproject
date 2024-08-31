5e1ba9b8180371b6d34ef539fba2a03934c516dc
KMG <ngl570202@gmail.com>
2024-08-31 14:52:49 +0900

    함수 밖에 정의하고 메인에서 넣기

--- Object orientation test/2024_08_31_1.cpp
+++ Object orientation test/2024_08_31_1.cpp
@@ -5,12 +5,11 @@ using namespace std;
 class parants
 {
 public:
-    void mom()
+    int age;
+    int lengh;
+    string name;
+    void state()
     {
-        int age = 10;
-        int lengh = 10;
-        string name = "김홍도";
-
         std::cout << "나이 : " << age << '\n'
                   << "키 : " << lengh << '\n'
                   << "이름 : " << name << endl;
@@ -19,7 +18,16 @@ public:
 int main()
 {
     parants Mom;
-    Mom.mom();
+    Mom.age = 50;
+    Mom.lengh = 160;
+    Mom.name = "장원영";
+    Mom.state();
+
+    parants Dad;
+    Dad.age = 55;
+    Dad.lengh = 170;
+    Dad.name = "차은우";
+    Dad.state();
 
     return 0;
 }
